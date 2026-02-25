b8a505561b6600b6490345f10dbe19f99f70292d
juan pablo maletti <juanpi.maletti@gmail.com>
2026-02-25 13:46:23 -0300

    Added description, filename, and placeholder for code snippets. Also added a button for saving gists. Css included.

--- lib/git_gist_clone_web/live/user_live/create_gist_live.ex
+++ lib/git_gist_clone_web/live/user_live/create_gist_live.ex
@@ -1,10 +1,22 @@
 defmodule GitGistCloneWeb.UserLive.CreateGistLive do
   use GitGistCloneWeb, :live_view
+  import Phoenix.HTML.Form
+  alias GitGistClone.{Gists, Gists.Gist}
 
-  def mount(_params, _session, socket)do
+
+  def mount(_params, _session, socket) do
+    scope = socket.assigns.current_scope
+
+    gist = %Gist{user_id: scope.user.id}
+
+    socket =
+      assign(socket,
+        form: to_form(
+          Gists.change_gist(scope, gist)
+        )
+      )
+
     {:ok, socket}
   end
-
-
 
 end
